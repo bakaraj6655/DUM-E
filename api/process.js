8a746162833b91864d23b33fd74bdc9238175cf2
bakaraj6655 <nirupamaashar114@gmail.com>
2026-05-27 02:55:52 +0530

    fix: updating js file3

--- api/process.js
+++ api/process.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   
   if (!prompt) return res.status(400).json({ error: 'No prompt' });
   try {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${process.env.GEMINI_KEY}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ contents: [{ parts: [{ text: prompt }] }] })
